868414948d1b2e80c4df8c507e6f3b0331d7a548
Marc Amorós <marc.amoros.trepat@gmail.com>
2024-02-26 18:24:47 +0100

    Adding locally run scripts

--- coupled_openfoam_calculix_v2/fluid-openfoam/run.sh
+++ coupled_openfoam_calculix_v2/fluid-openfoam/run.sh
@@ -4,6 +4,6 @@ set -e -u
 blockMesh
 touch fluid-openfoam.foam
 
-../../tutorials/tools/run-openfoam.sh "$@"
-. ../../tutorials/tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
+./run-openfoam.sh "$@"
+. ./openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
 
